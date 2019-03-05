85f83598b548cc5150fc4e0e8049fdebae905e4d
Pinchu <py416@ic.ac.uk>
2019-03-05 12:17:58 +0000

    integrated orocos kdl lib

---
+++ src/Test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Pinchu on 2019/2/18.
+//
+#include <iostream>
+#include "include/Test.h"
+
+using namespace std;
+
+int main() {
+    cout << "Simulation starts here." << endl;
+    return 0;
+}
---
+++ src/WorldFrame.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pinchu on 2019/2/18.
+//
+
+#include "include/WorldFrame.h"
