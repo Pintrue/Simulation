b934ed3f7a327ca7eccff7311d8015f27cf843bf
Pintrue <y451059931@icloud.com>
2019-04-18 18:22:49 +0800

    ready to start traj

--- src/Sim.cpp
+++ src/Sim.cpp
@@ -5,18 +5,18 @@ using namespace std;
 using namespace KDL;
 
 int main() {
-    KinematicsModel km;
+    Sim sim;
 
     double pos[3] = {0.0, 0.0, 0.0};
-    km.init(pos);
+    sim._km.init(pos);
 
     JntArray jointAngles = JntArray(3);
 	jointAngles(0) = M_PI / 2;			// Joint 1
-	jointAngles(1) = 0;			// Joint 2
-	jointAngles(2) = -(80.0/180.0*M_PI);			// Joint 3
+	jointAngles(1) = 0;			        // Joint 2
+	jointAngles(2) = -(80.0/180.0*M_PI);// Joint 3
 
     Frame eeFrame;
-    km.jntsToCart(jointAngles, eeFrame);
+    sim._km.jntsToCart(jointAngles, eeFrame);
     print_frame(eeFrame);
     cout << "Done" << endl;
 }
