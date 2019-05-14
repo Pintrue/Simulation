760909168c574a4b899e122385f2fd46f658873d
xj616 <xj616@ic.ac.uk>
2019-05-14 17:42:05 +0100

    merged C rl lib, adjusted cmake for compile on macos

--- src/C_api.hpp
+++ src/C_api.hpp
@@ -10,14 +10,18 @@
 #include "robot_reinforcement_learning/C/main.h"
 #include "Sim.hpp"
 
-extern "C" int initEnv();
+extern "C" int initEnv(int act_dim);
 
 extern "C" double rand_uniform(double low, double high);
 // state: angles, target/coord, ee/coord, if succeed = 1*10 double
-extern "C" matrix_t* resetState(int randAngle, int destPos);
+extern "C" matrix_t* resetState(int randAngle, int destPos, int state_dim, int act_dim);
 
 // three angles to move about, NOT the angles to be moved to
 // action::1*3 double
-extern "C" matrix_t* step(matrix_t* action);
+extern "C" matrix_t* step(matrix_t* action, int state_dim, int act_dim);
+
+extern "C" void closeEnv(int state_dim, int act_dim);
+
+extern "C" matrix_t* random_action(int state_dim, int act_dim);
 
 #endif
